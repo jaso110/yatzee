0df8fb1355af9dadc912632b822efd639bad5c9c
Jan Sørensen <jaso110@gmail.com>
2022-08-09 13:01:53 +0200

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const dice_player_one = [1, 2, 3, 4, 5, 6];
-const dice_player_two = [1, 2, 3, 4, 5, 6];
-const dice_player_three = [1, 2, 3, 4, 5, 6];
-const dice_player_four = [1, 2, 3, 4, 5, 6];
-const dice_player_five = [1, 2, 3, 4, 5, 6];
+const dice_one = [1, 2, 3, 4, 5, 6];
+const dice_two = [1, 2, 3, 4, 5, 6];
+const dice_three = [1, 2, 3, 4, 5, 6];
+const dice_four = [1, 2, 3, 4, 5, 6];
+const dice_five = [1, 2, 3, 4, 5, 6];
 
 document.querySelector('#player1-name').addEventListener("click", playerOne);
 document.querySelector('#player2-name').addEventListener("click", playerTwo);
@@ -28,14 +28,14 @@ function playerTwo() {
 }
 
 function rollDiceOne() {
-  for (let i = dice_player_one.length -1; i > 0; i--) {
+  for (let i = dice_one.length -1; i > 0; i--) {
     let j = Math.floor(Math.random() * i);
-    let k = dice_player_one[i];
-    dice_player_one[i] = dice_player_one[j];
-    dice_player_one[j] = k;
+    let k = dice_one[i];
+    dice_one[i] = dice_one[j];
+    dice_one[j] = k;
   }
 
-  let dice_number1 = dice_player_one[0];
+  let dice_number1 = dice_one[0];
 
   console.log(dice_number1);
 
@@ -43,14 +43,14 @@ function rollDiceOne() {
 }
 
 function rollDiceTwo() {
-  for (let i = dice_player_two.length -1; i > 0; i--) {
+  for (let i = dice_two.length -1; i > 0; i--) {
     let j = Math.floor(Math.random() * i);
-    let k = dice_player_two[i];
-    dice_player_two[i] = dice_player_two[j];
-    dice_player_two[j] = k;
+    let k = dice_two[i];
+    dice_two[i] = dice_two[j];
+    dice_two[j] = k;
   }
 
-  let dice_number2 = dice_player_two[0];
+  let dice_number2 = dice_two[0];
 
   console.log(dice_number2);
 
@@ -58,14 +58,14 @@ function rollDiceTwo() {
 }
 
 function rollDiceThree() {
-  for (let i = dice_player_three.length -1; i > 0; i--) {
+  for (let i = dice_three.length -1; i > 0; i--) {
     let j = Math.floor(Math.random() * i);
-    let k = dice_player_three[i];
-    dice_player_three[i] = dice_player_three[j];
-    dice_player_three[j] = k;
+    let k = dice_three[i];
+    dice_three[i] = dice_three[j];
+    dice_three[j] = k;
   }
 
-  let dice_number3 = dice_player_three[0];
+  let dice_number3 = dice_three[0];
 
   console.log(dice_number3);
 
@@ -73,14 +73,14 @@ function rollDiceThree() {
 }
 
 function rollDiceFour() {
-  for (let i = dice_player_four.length -1; i > 0; i--) {
+  for (let i = dice_four.length -1; i > 0; i--) {
     let j = Math.floor(Math.random() * i);
-    let k = dice_player_four[i];
-    dice_player_four[i] = dice_player_four[j];
-    dice_player_four[j] = k;
+    let k = dice_four[i];
+    dice_four[i] = dice_four[j];
+    dice_four[j] = k;
   }
 
-  let dice_number4 = dice_player_four[0];
+  let dice_number4 = dice_four[0];
 
   console.log(dice_number4);
 
@@ -88,14 +88,14 @@ function rollDiceFour() {
 }
 
 function rollDiceFive() {
-  for (let i = dice_player_five.length -1; i > 0; i--) {
+  for (let i = dice_five.length -1; i > 0; i--) {
     let j = Math.floor(Math.random() * i);
-    let k = dice_player_five[i];
-    dice_player_five[i] = dice_player_five[j];
-    dice_player_five[j] = k;
+    let k = dice_five[i];
+    dice_five[i] = dice_five[j];
+    dice_five[j] = k;
   }
 
-  let dice_number5 = dice_player_five[0];
+  let dice_number5 = dice_five[0];
 
   console.log(dice_number5);
 
